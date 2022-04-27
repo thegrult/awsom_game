dea54250d1e77a8fc8f2614982a5114f4e9db360
thegrult <thegrultgamer05@gmail.com>
2022-04-27 22:30:52 +0200

    noticed that after a while protagonist doesn't get damaged: it's because he's "dead", even thought there aren't any death mechanics, its entity's state is "dead" so it doesn't get damaged.

--- awsom_game/Protagonist.cpp
+++ awsom_game/Protagonist.cpp
@@ -3,7 +3,7 @@
 Protagonist::Protagonist( Vec2 spawnPos, SDL_Renderer* renderer )
 	:
 	sprite( "imgs/w_g_sprites.png", renderer ),
-	entity( spawnPos, {0,0}, 32, 32, 8, 8, sprite, renderer, { 12, 20, 24, 32 } )
+	entity( spawnPos, {0,0}, 32, 32, 8, 9, sprite, renderer, { 12, 20, 24, 32 } )
 {}
 
 void Protagonist::Update( float dt, const Uint8* kbd )
@@ -13,25 +13,25 @@ void Protagonist::Update( float dt, const Uint8* kbd )
 		coolDownTimer -= dt;
 	else coolDownTimer = 0;
 
-	Vec2 direc = { 0.0f,0.0f };
+	Vei2 direc = { 0,0 };
 
 	if (kbd[SDL_SCANCODE_W]) {
-		direc.y -= 1.0f;
+		direc.y -= 1;
 	}
 	if (kbd[SDL_SCANCODE_S]) {
-		direc.y += 1.0f;
+		direc.y += 1;
 	}
 	if (kbd[SDL_SCANCODE_A]) {
-		direc.x -= 1.0f;
+		direc.x -= 1;
 	}
 	if (kbd[SDL_SCANCODE_D]) {
-		direc.x += 1.0f;
+		direc.x += 1;
 	}
 
-	SetDirection( direc );
+	SetDirection( (Vec2)direc );
 
-	if (direc != Vec2( 0.0f, 0.0f )) {
-		dir = direc;
+	if (direc != Vei2( 0.0f, 0.0f )) {
+		dir = (Vec2)direc;
 	}
 }
 
@@ -39,6 +39,7 @@ void Protagonist::SetDirection( const Vec2& dir )
 {
 	entity.SetDirection( dir );
 
+	//selects the animation based on direction (right = +1, left = +2, down = +3, up = +6), when dmged +9 
 	int animindex = 0;
 
 	if (dir.y < -0.05f) {
